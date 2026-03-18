682ed713aa4468ac5d122a5dddb8e286dde3adb4
GovindGaur32 <govindgaur327@gmail.com>
2026-03-19 00:30:58 +0530

    Refactor code structure for improved readability and maintainability

--- src/Assets/Projects/Pro.js
+++ src/Assets/Projects/Pro.js
@@ -1,8 +1,9 @@
 import img1 from "./Screenshot 2025-01-20 141115.png";
 import img2 from "./Screenshot 2025-01-20 220512.png";
 import img3 from "./Screenshot 2025-01-20 221645.png";
+import img4 from "./Screenshot 2026-03-18 192538.png";
 
-const images = { img1, img2, img3 };
+const images = { img1, img2, img3, img4 };
 
 export default images;
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import images from '../../Assets/Projects/Pro';
 const img1 = images.img1;
 const img2 = images.img2;
 const img3 = images.img3;
-
+const img4 = images.img4;
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -33,13 +33,13 @@ function Projects() {
 
           
 
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" onClick={() => window.open("https://prescript0-frontend.vercel.app/", "_blank")}>
             <ProjectCard
               imgPath={img1}
               isBlog={false}
               title="Prescripto (hospital management system)"
               description="A hospital management system build with react.js, Material-UI, and Mongodb. Have features which allows user to book an appointment, view their appointment history, and also allows admin to view all the appointments and manage them."
-              ghLink="https://github.com/GovindGaur32/prescripto-full-stack"
+              ghLink="https://github.com/GovindGaur32/Prescript0-Frontend"
                             
             />
           </Col>
@@ -51,6 +51,16 @@ function Projects() {
               title="Music app (spotify-clone)"
               description="A music app build with react.js, Material-UI, Express.js. Have features which allows user to search for songs, artists, albums, and playlists. User can also play the preview of the song and can also play the full song on Spotify."
               ghLink="https://github.com/GovindGaur32/Spotify-Clone"
+              
+            />
+          </Col>
+          <Col md={4} className="project-card"onClick={()=>window.open("https://trackfit-ai.vercel.app/", "_blank")}>
+            <ProjectCard
+              imgPath={img4}
+              isBlog={false}
+              title="Fitness app"
+              description="A fitness app build with react.js, Material-UI, Express.js. Have features which allows user to search for exercises, create workout plans, and track their progress."
+              ghLink="https://github.com/GovindGaur32/FitnessTracker"
               
             />
           </Col>
